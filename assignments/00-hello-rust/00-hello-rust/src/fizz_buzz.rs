a9dda6e09cd4faef6285939f51091622e202d9ca
Rixhee <rishitbansals@gmail.com>
2024-09-12 13:02:51 -0400

    Finished Fizz buzz

--- assignments/00-hello-rust/00-hello-rust/src/fizz_buzz.rs
+++ assignments/00-hello-rust/00-hello-rust/src/fizz_buzz.rs
@@ -1,1 +1,27 @@
-pub fn print_fizz_buzz(max_num: u32) {}
+//! FizzBuzz implementation
+
+pub fn print_fizz_buzz(max_num: u32) {
+    /// This function prints runs through the numbers from 1 to max_num, and
+    /// print "Fizz" if the number is divisible by 3, "Buzz" if the number is
+    /// divisible by 5, and "FizzBuzz" if the number is divisible by both 3 and
+    /// 5. Otherwise, it prints the number itself.
+    ///
+    /// # Arguments
+    ///
+    /// * `max_num`: u32 - The maximum number to check
+    ///
+    /// # Returns
+    ///
+    /// None
+    for i in 1..=max_num {
+        if i % 15 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
