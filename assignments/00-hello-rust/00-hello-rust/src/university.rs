b40210158fe91979cba9ab88fe3c5217bb4f6940
Rixhee <rishitbansals@gmail.com>
2024-09-15 20:15:00 -0400

    Implemented university.rs
    Added the new olin students into the struct; wrote get_average_gpa,
    get_num_excel_students_for_class, and get_best_class functions; and
    added documentation

--- assignments/00-hello-rust/00-hello-rust/src/university.rs
+++ assignments/00-hello-rust/00-hello-rust/src/university.rs
@@ -12,7 +12,7 @@ struct Student {
     gpa: f32,
 }
 
-const OLIN_STUDENTS: [Student; 5] = [
+const OLIN_STUDENTS: [Student; 8] = [
     Student {
         name: "Alice",
         class_year: ClassYear::Senior,
@@ -38,13 +38,93 @@ const OLIN_STUDENTS: [Student; 5] = [
         class_year: ClassYear::Senior,
         gpa: 0.0,
     },
+    Student {
+        name: "Anna",
+        class_year: ClassYear::FirstYear,
+        gpa: 4.0,
+    },
+    Student {
+        name: "Hannah",
+        class_year: ClassYear::FirstYear,
+        gpa: 4.0,
+    },
+    Student {
+        name: "Lorin",
+        class_year: ClassYear::Junior,
+        gpa: 3.6,
+    },
 ];
 
-fn get_average_gpa() -> f32 {}
+/// Returns the average GPA of the students in the university
+///
+/// # Returns
+///
+/// An `f32` representing the average GPA
+fn get_average_gpa() -> f32 {
+    let mut sum: f32 = 0.0;
+    let mut counter: u32 = 0;
 
-fn get_num_excel_students_for_class(class_year: ClassYear) -> u32 {}
+    // Calculate the sum of all students' GPAs except for the first year
+    for student in OLIN_STUDENTS {
+        if student.class_year != ClassYear::FirstYear {
+            sum += student.gpa;
+            counter += 1;
+        }
+    }
 
-fn get_best_class() -> ClassYear {}
+    return sum / counter as f32;
+}
+
+/// Returns the number of students in a class year who have a gpa greater
+/// than or equal to the average gpa of all students
+///
+/// # Arguments
+///
+/// * `class_year`: A `ClassYear` enum representing the class year
+///
+/// # Returns
+///
+/// An `u32` representing the number of students in the class year
+fn get_num_excel_students_for_class(class_year: ClassYear) -> u32 {
+    let mut counter: u32 = 0;
+
+    // Count the number of students who have a gpa greater than or equal to the average gpa
+    for student in OLIN_STUDENTS {
+        if student.class_year == class_year && student.gpa >= get_average_gpa() {
+            counter += 1;
+        }
+    }
+
+    return counter;
+}
+
+/// Returns the class year with the most excel students
+///
+/// # Returns
+///
+/// A `ClassYear` enum representing the class year with the most excel students
+fn get_best_class() -> ClassYear {
+    let mut most_excel_students: u32 = 0;
+    let mut best_class: ClassYear = ClassYear::FirstYear;
+
+    // Find the class year with the most excel students
+    for class_year in [
+        ClassYear::Senior,
+        ClassYear::Junior,
+        ClassYear::Sophomore,
+        ClassYear::FirstYear,
+    ]
+    .into_iter()
+    {
+        let current_excel_students = get_num_excel_students_for_class(class_year);
+        if current_excel_students > most_excel_students {
+            most_excel_students = current_excel_students;
+            best_class = class_year;
+        }
+    }
+
+    return best_class;
+}
 
 // Do not modify below here
 #[cfg(test)]
