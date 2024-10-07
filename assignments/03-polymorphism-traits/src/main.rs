f6441c29556613ddf693b3fa33b9fef816071c63
Rixhee <rishitbansals@gmail.com>
2024-10-06 20:54:14 -0400

    Finished main.rs
    Imported the functions i wrote to implement grep and then
    added them in the main function

--- assignments/03-polymorphism-traits/src/main.rs
+++ assignments/03-polymorphism-traits/src/main.rs
@@ -1,7 +1,16 @@
 use anyhow::Result;
 use clap::Parser;
 use colored::Color;
+use std::io::stdout;
 use std::path::PathBuf;
+
+mod colored_output;
+mod find_match;
+mod input;
+
+use colored_output::colored_output;
+use find_match::filter_lines;
+use input::get_lines_from_input;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -25,5 +34,10 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
     println!("{:?}", args);
+
+    let lines = get_lines_from_input(args.file)?;
+    let filter_lines = filter_lines(args.needle, lines, args.ignore_case, args.invert_match)?;
+    let _ = colored_output(filter_lines, &mut stdout(), args.color);
+
     Ok(())
 }
